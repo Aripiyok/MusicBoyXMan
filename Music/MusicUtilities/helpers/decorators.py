c1c1104b237ce78bf22d14fc8f8dd425247df6df
Aripiyok <86340085+Aripiyok@users.noreply.github.com>
2021-11-25 01:30:23 +0700

    Update decorators.py

--- Music/MusicUtilities/helpers/decorators.py
+++ Music/MusicUtilities/helpers/decorators.py
@@ -2,11 +2,29 @@ from typing import Callable
 from pyrogram import Client
 from pyrogram.types import Message
 
+from MusicUtilities.config import SUDO_USERS
+from MusicUtilities.helpers.admins import get_administrators
+
+
 def errors(func: Callable) -> Callable:
     async def decorator(client: Client, message: Message):
         try:
             return await func(client, message)
         except Exception as e:
             await message.reply(f'{type(e).__name__}: {e}', False)
+
+    return decorator
+
+
+def authorized_users_only(func: Callable) -> Callable:
+    async def decorator(client: Client, message: Message):
+        if message.from_user.id in SUDO_USERS:
+            return await func(client, message)
+
+        administrators = await get_administrators(message.chat)
+
+        for administrator in administrators:
+            if administrator == message.from_user.id:
+                return await func(client, message)
 
     return decorator
