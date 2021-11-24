9bd361bb2a3ffedef0147a407f5cfe90b6d537b7
Aripiyok <86340085+Aripiyok@users.noreply.github.com>
2021-11-25 01:58:48 +0700

    Update decorators.py

--- Music/MusicUtilities/helpers/decorators.py
+++ Music/MusicUtilities/helpers/decorators.py
@@ -2,7 +2,7 @@ from typing import Callable
 from pyrogram import Client
 from pyrogram.types import Message
 
-from MusicUtilities.config import SUDO_USERS
+from Music.config import SUDO_USERS
 from MusicUtilities.helpers.admins import get_administrators
 
 
