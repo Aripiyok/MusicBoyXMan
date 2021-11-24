87e98f10afbf740279824ad2f1f537ba691db02c
Aripiyok <86340085+Aripiyok@users.noreply.github.com>
2021-11-25 03:40:47 +0700

    Update administrator.py

--- Music/MusicUtilities/helpers/administrator.py
+++ Music/MusicUtilities/helpers/administrator.py
@@ -3,7 +3,8 @@ from functools import wraps
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 from pyrogram.types import Message
 from Music import app, SUDOERS
-from Music.Plugins.admins import member_permissions
+from Music.function.admins import set
+
 
 
 async def authorised(message):
