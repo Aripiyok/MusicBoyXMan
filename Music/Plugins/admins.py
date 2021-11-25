9cd599286990cc5d45a4d098d83285caf5a99d93
Aripiyok <86340085+Aripiyok@users.noreply.github.com>
2021-11-26 04:50:24 +0700

    Update admins.py

--- Music/Plugins/admins.py
+++ Music/Plugins/admins.py
@@ -119,7 +119,6 @@ async def stop_cmd(_, message):
             await message.reply_text("No more music in __Queue__ \n\nLeaving Voice Chat")
             await music.pytgcalls.leave_group_call(chat_id)
             return  
-        else:
                 mystic = await message.reply_text("Music is currently playing Playlist...\n\nDownloading Next Music From Playlist....")
                 url = (f"https://www.youtube.com/watch?v={afk}")
                 try:
