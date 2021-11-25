057103c98716969d4f521c28d58e55c11a2fbb52
Aripiyok <86340085+Aripiyok@users.noreply.github.com>
2021-11-26 03:29:51 +0700

    Update play.py

--- Music/Plugins/play.py
+++ Music/Plugins/play.py
@@ -132,7 +132,8 @@ async def play(_, message: Message):
     if message.chat.id in DISABLED_GROUPS:
         return
     if message.sender_chat:
-        return await message.reply_text("You're an __Anonymous Admin__!\nRevert back to User Account From Admin Rights.
+        return 
+    if await message.reply_text("You're an __Anonymous Admin__!\nRevert back to User Account From Admin Rights.
     user_id = message.from_user.id
     chat_title = message.chat.title
     username = message.from_user.first_name
