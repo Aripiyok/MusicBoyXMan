b04638112bdf904afecde05092b7c2e47b3f3249
Aripiyok <86340085+Aripiyok@users.noreply.github.com>
2021-11-26 04:59:24 +0700

    Update admins.py

--- Music/Plugins/admins.py
+++ Music/Plugins/admins.py
@@ -118,9 +118,8 @@ async def stop_cmd(_, message):
             await remove_active_chat(chat_id)
             await message.reply_text("No more music in __Queue__ \n\nLeaving Voice Chat")
             await music.pytgcalls.leave_group_call(chat_id)
-            return  
-    else:        
-        await message.reply_text("Music is currently playing Playlist...\n\nDownloading Next Music From Playlist....")
+            return         
+            await message.reply_text("Music is currently playing Playlist...\n\nDownloading Next Music From Playlist....")
                 url = (f"https://www.youtube.com/watch?v={afk}")
                 try:
                     with yt_dlp.YoutubeDL(ytdl_opts) as ytdl:
