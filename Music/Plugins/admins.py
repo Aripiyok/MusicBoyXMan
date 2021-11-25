ab0432613f15b3c61a5894f1479d335afaaf6ec6
Aripiyok <86340085+Aripiyok@users.noreply.github.com>
2021-11-26 05:53:43 +0700

    Update admins.py

--- Music/Plugins/admins.py
+++ Music/Plugins/admins.py
@@ -107,8 +107,129 @@ async def stop_cmd(_, message):
     if message.sender_chat:
         return await message.reply_text("You're an __Anonymous Admin__!\nRevert back to User Account.") 
     permission = "can_manage_voice_chats"
+    m = await admins(permission, message)
+    if m == 1:
+        return
     checking = message.from_user.mention
     chat_id = message.chat.id
     chat_title = message.chat.title
     if not await is_active_chat(chat_id):
-           await message.reply_text("Nothing's playing on Music")
+        await message.reply_text("Nothing's playing on Music")
+    else:
+        task_done(chat_id)
+        if is_empty(chat_id):
+            await remove_active_chat(chat_id)
+            await message.reply_text("No more music in __Queue__ \n\nLeaving Voice Chat")
+            await music.pytgcalls.leave_group_call(chat_id)
+            return  
+        else:
+            afk = get(chat_id)['file']
+            f1 = (afk[0])
+            f2 = (afk[1])
+            f3 = (afk[2])
+            finxx = (f"{f1}{f2}{f3}")
+            if str(finxx) != "raw":   
+                mystic = await message.reply_text("Music is currently playing Playlist...\n\nDownloading Next Music From Playlist....")
+                url = (f"https://www.youtube.com/watch?v={afk}")
+                try:
+                    with yt_dlp.YoutubeDL(ytdl_opts) as ytdl:
+                        x = ytdl.extract_info(url, download=False)
+                except Exception as e:
+                    return await mystic.edit(f"Failed to download this video.\n\n**Reason**:{e}") 
+                title = (x["title"])
+                videoid = afk
+                def my_hook(d):
+                    if d['status'] == 'downloading':
+                        percentage = d['_percent_str']
+                        per = (str(percentage)).replace(".","", 1).replace("%","", 1)
+                        per = int(per)
+                        eta = d['eta']
+                        speed = d['_speed_str']
+                        size = d['_total_bytes_str']
+                        bytesx = d['total_bytes']
+                        if str(bytesx) in flex:
+                            pass
+                        else:
+                            flex[str(bytesx)] = 1
+                        if flex[str(bytesx)] == 1:
+                            flex[str(bytesx)] += 1
+                            sedtime.sleep(1)
+                            mystic.edit(f"Downloading {title[:50]}\n\n**FileSize:** {size}\n**Downloaded:** {percentage}\n**Speed:** {speed}\n**ETA:** {eta} sec")
+                        if per > 500:    
+                            if flex[str(bytesx)] == 2:
+                                flex[str(bytesx)] += 1
+                                sedtime.sleep(0.5)
+                                mystic.edit(f"Downloading {title[:50]}...\n\n**FileSize:** {size}\n**Downloaded:** {percentage}\n**Speed:** {speed}\n**ETA:** {eta} sec")
+                                print(f"[{videoid}] Downloaded {percentage} at a speed of {speed} in {chat_title} | ETA: {eta} seconds")
+                        if per > 800:    
+                            if flex[str(bytesx)] == 3:
+                                flex[str(bytesx)] += 1
+                                sedtime.sleep(0.5)
+                                mystic.edit(f"Downloading {title[:50]}....\n\n**FileSize:** {size}\n**Downloaded:** {percentage}\n**Speed:** {speed}\n**ETA:** {eta} sec")
+                                print(f"[{videoid}] Downloaded {percentage} at a speed of {speed} in {chat_title} | ETA: {eta} seconds")
+                        if per == 1000:    
+                            if flex[str(bytesx)] == 4:
+                                flex[str(bytesx)] = 1
+                                sedtime.sleep(0.5)
+                                mystic.edit(f"Downloading {title[:50]}.....\n\n**FileSize:** {size}\n**Downloaded:** {percentage}\n**Speed:** {speed}\n**ETA:** {eta} sec") 
+                                print(f"[{videoid}] Downloaded {percentage} at a speed of {speed} in {chat_title} | ETA: {eta} seconds")
+                loop = asyncio.get_event_loop()
+                xxx = await loop.run_in_executor(None, download, url, my_hook)
+                file = await convert(xxx)
+                await music.pytgcalls.change_stream(
+                    chat_id, 
+                    InputStream(
+                        InputAudioStream(
+                            file,
+                        ),
+                    ),
+                )
+                thumbnail = (x["thumbnail"])
+                duration = (x["duration"])
+                duration = round(x["duration"] / 60)
+                theme = random.choice(themes)
+                ctitle = (await app.get_chat(chat_id)).title
+                ctitle = await CHAT_TITLE(ctitle)
+                f2 = open(f'search/{afk}id.txt', 'r')        
+                userid =(f2.read())
+                thumb = await gen_thumb(thumbnail, title, userid, theme, ctitle)
+                user_id = userid
+                buttons = play_markup(videoid, user_id)
+                await mystic.delete()
+                semx = await app.get_users(userid)
+                await message.reply_photo(
+                photo= thumb,
+                reply_markup=InlineKeyboardMarkup(buttons),    
+                caption=(f"<b>__Skipped Voice Chat__</b>\n\n🎥<b>__Started Playing:__ </b>[{title[:25]}]({url}) \n⏳<b>__Duration:__</b> {duration} Mins\n👤**__Requested by:__** {semx.mention}")
+            )   
+                os.remove(thumb)
+            else:      
+                await music.pytgcalls.change_stream(
+                    chat_id, 
+                    InputStream(
+                        InputAudioStream(
+                            afk,
+                        ),
+                    ),
+                )
+                _chat_ = ((str(afk)).replace("_","", 1).replace("/","", 1).replace(".","", 1))
+                f2 = open(f'search/{_chat_}title.txt', 'r')        
+                title =(f2.read())
+                f3 = open(f'search/{_chat_}duration.txt', 'r')        
+                duration =(f3.read())
+                f4 = open(f'search/{_chat_}username.txt', 'r')        
+                username =(f4.read())
+                f4 = open(f'search/{_chat_}videoid.txt', 'r')        
+                videoid =(f4.read())
+                user_id = 1
+                videoid = str(videoid)
+                if videoid == "smex1":
+                    buttons = audio_markup(videoid, user_id)
+                else:
+                    buttons = play_markup(videoid, user_id)
+                await message.reply_photo(
+                photo=f"downloads/{_chat_}final.png",
+                reply_markup=InlineKeyboardMarkup(buttons),
+                caption=f"<b>__Skipped Voice Chat__</b>\n\n🎥<b>__Started Playing:__</b> {title} \n⏳<b>__Duration:__</b> {duration} \n👤<b>__Requested by:__ </b> {username}",
+                )
+                return
