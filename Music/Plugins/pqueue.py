63c9bc77ea3bb1a877fc27212b864dacb2aa2686
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2021-12-20 05:30:28 +0700

    Update pqueue.py

--- Music/Plugins/pqueue.py
+++ Music/Plugins/pqueue.py
@@ -1,34 +1,147 @@
-from Music import BOT_USERNAME
-from pyrogram.types import (
-    CallbackQuery,
-    InlineKeyboardButton,
-    InlineKeyboardMarkup,
-    Message,
-)
+import asyncio
+import os
+import shutil
+import subprocess
+from sys import version as pyver
+
+from config import get_queue
 from pyrogram import Client, filters
-from Music.MusicUtilities.tgcallsrun import queues
+from pyrogram.types import Message
+
+from Music import SUDOERS, app, db_mem,
+from Music.MusicUtilities.tgcallsrun.music import ASS_ACC as userbot
+from Music.MusicUtilities.database.queue import get_active_chats, is_active_chat
+
+get_queue = []
+
+@app.on_message(filters.command("queue"))
+async def activevc(_, message: Message):
+    global get_queue
+    if await is_active_chat(message.chat.id):
+        mystic = await message.reply_text("Please Wait... Getting Queue..")
+        dur_left = db_mem[message.chat.id]["left"]
+        duration_min = db_mem[message.chat.id]["total"]
+        got_queue = get_queue.get(message.chat.id)
+        if not got_queue:
+            await mystic.edit(f"Nothing in Queue")
+        fetched = []
+        for get in got_queue:
+            fetched.append(get)
+
+        ### Results
+        current_playing = fetched[0][0]
+        user_name = fetched[0][1]
+
+        msg = "**Queued List**\n\n"
+        msg += "**Currently Playing:**"
+        msg += "\n▶️" + current_playing[:30]
+        msg += f"\n   ╚By:- {user_name}"
+        msg += f"\n   ╚Duration:- Remaining `{dur_left}` out of `{duration_min}` Mins."
+        fetched.pop(0)
+        if fetched:
+            msg += "\n\n"
+            msg += "**Up Next In Queue:**"
+            for song in fetched:
+                name = song[0][:30]
+                usr = song[1]
+                dur = song[2]
+                msg += f"\n⏸️{name}"
+                msg += f"\n   ╠Duration : {dur}"
+                msg += f"\n   ╚Requested by : {usr}\n"
+        if len(msg) > 4096:
+            await mystic.delete()
+            filename = "queue.txt"
+            with open(filename, "w+", encoding="utf8") as out_file:
+                out_file.write(str(msg.strip()))
+            await message.reply_document(
+                document=filename,
+                caption=f"**OUTPUT:**\n\n`Queued List`",
+                quote=False,
+            )
+            os.remove(filename)
+        else:
+            await mystic.edit(msg)
+    else:
+        await message.reply_text(f"Nothing in Queue")
 
 
-keyboard = InlineKeyboardMarkup(
-    [[InlineKeyboardButton("🗑 Close", callback_data="cls")]]
-)
+@app.on_message(filters.command("activevc") & filters.user(SUDOERS))
+async def activevc(_, message: Message):
+    served_chats = []
+    try:
+        chats = await get_active_chats()
+        for chat in chats:
+            served_chats.append(int(chat["chat_id"]))
+    except Exception as e:
+        await message.reply_text(f"**Error:-** {e}")
+    text = ""
+    j = 0
+    for x in served_chats:
+        try:
+            title = (await app.get_chat(x)).title
+        except Exception:
+            title = "Private Group"
+        if (await app.get_chat(x)).username:
+            user = (await app.get_chat(x)).username
+            text += (
+                f"<b>{j + 1}.</b>  [{title}](https://t.me/{user})[`{x}`]\n"
+            )
+        else:
+            text += f"<b>{j + 1}. {title}</b> [`{x}`]\n"
+        j += 1
+    if not text:
+        await message.reply_text("No Active Voice Chats")
+    else:
+        await message.reply_text(
+            f"**Active Voice Chats:-**\n\n{text}",
+            disable_web_page_preview=True,
+        )
 
 
-@Client.on_message(filters.command(["playlist", f"playlist@{BOT_USERNAME}", "queue", f"queue@{BOT_USERNAME}"]) & filters.group)
-async def playlist(client, m: Message):
-   chat_id = m.chat.id
-   if chat_id in queues:
-      chat_queue = Queue.get(chat_id)
-      if len(chat_queue)==1:
-         await m.reply(f"💡 **Currently Streaming:**\n\n• [{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}`", reply_markup=keyboard, disable_web_page_preview=True)
-      else:
-         QUE = f"💡 **Currently Streaming:**\n\n• [{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}` \n\n**📖 Queue List:**\n"
-         l = len(chat_queue)
-         for x in range (1, l):
-            han = chat_queue[x][0]
-            hok = chat_queue[x][2]
-            hap = chat_queue[x][3]
-            QUE = QUE + "\n" + f"**#{x}** - [{han}]({hok}) | `{hap}`"
-         await m.reply(QUE, reply_markup=keyboard, disable_web_page_preview=True)
-   else:
-      await m.reply("❌ **nothing is currently streaming.**")
+@app.on_message(filters.command("joinassistant") & filters.user(SUDOERS))
+async def basffy(_, message):
+    if len(message.command) != 2:
+        await message.reply_text(
+            "**Usage:**\n/joinassistant [Chat Username or Chat ID]"
+        )
+        return
+    chat = message.text.split(None, 2)[1]
+    try:
+        await userbot.join_chat(chat)
+    except Exception as e:
+        await message.reply_text(f"Failed\n**Possible reason could be**:{e}")
+        return
+    await message.reply_text("Joined.")
+
+
+@app.on_message(filters.command("leavebot") & filters.user(SUDOERS))
+async def baaaf(_, message):
+    if len(message.command) != 2:
+        await message.reply_text(
+            "**Usage:**\n/leavebot [Chat Username or Chat ID]"
+        )
+        return
+    chat = message.text.split(None, 2)[1]
+    try:
+        await app.leave_chat(chat)
+    except Exception as e:
+        await message.reply_text(f"Failed\n**Possible reason could be**:{e}")
+        print(e)
+        return
+    await message.reply_text("Bot has left the chat successfully")
+
+
+@app.on_message(filters.command("leaveassistant") & filters.user(SUDOERS))
+async def baujaf(_, message):
+    if len(message.command) != 2:
+        await message.reply_text(
+            "**Usage:**\n/leave [Chat Username or Chat ID]"
+        )
+        return
+    chat = message.text.split(None, 2)[1]
+    try:
+        await userbot.leave_chat(chat)
+    except Exception as e:
+        await message.reply_text(f"Failed\n**Possible reason could be**:{e}")
+        return
+    await message.reply_text("Left.")
